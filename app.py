bf8bbee4d519d79c6e087d7ad22597dd0b3ecf5b
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-20 18:04:40 +0530

    using flakenv

--- app.py
+++ app.py
@@ -1,54 +1,51 @@
 from flask import Flask, request
+from db import items, stores
+import uuid
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "chair",
-                "price": 15.99,
-            }
-        ]
-    }
-]
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_id = uuid.uuid4().hex
+    new_store = {**request_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return {"store_updated": store}, 201
-    return {"message": "Store not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+    item_id = uuid.uuid4().hex
+    new_item = {**item_data, "id": item_id}
+    items[item_id] = new_item # type: ignore
+    return new_item, 200
 
 
-@app.get("/store/<string:name>/")
-def get_store_items(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"store": store}, 200
-    return {"message": "Store not found"}, 404
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 
-@app.get("/store/<string:name>/items")
-def get_items_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"store_items": store["items"]}, 200
-    return {"message": "Store not found"}, 404
+@app.get("/store/<string:store_id>/")
+def get_store_items(store_id):
+    try:
+        return {"store": stores[store_id]}, 200
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+
+@app.get("/item/<string:item_id>") # type: ignore
+def get_items_in_store(item_id):
+    try:
+        return items[item_id], 200
+    except KeyError:
+        return {"message": "Item not found"}, 404
