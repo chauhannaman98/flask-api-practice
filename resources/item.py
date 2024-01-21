5ba920ee4dece26270edb7b4836cc470e2029bc7
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-21 19:48:11 +0530

    Added response schema

--- resources/item.py
+++ resources/item.py
@@ -9,6 +9,7 @@ blp = Blueprint("items", __name__, description="Operations in Items")
 
 @blp.route('/item/<string:item_id>')
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id], 200
@@ -23,6 +24,7 @@ class Item(MethodView):
             abort(404, message="Unable to find the item.")
 
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
@@ -35,10 +37,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
     @blp.arguments(ItemSchema)
+    @blp.response(202, ItemSchema)
     def post(self, item_data):
         for item in items.values():
             if (
