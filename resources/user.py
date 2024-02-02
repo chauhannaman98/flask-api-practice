d19f49e5420f04c5b091123ce5de4aa7afb2daf9
Naman Chauhan <chauhannaman98@gmail.com>
2024-02-03 02:05:34 +0530

    Added authentication

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,10 @@
+from email.mime import message
+from venv import create
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
+from datetime import datetime, timedelta
 
 from db import db
 from models import UserModel
@@ -23,6 +27,20 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "Registration successful"}, 201
+    
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+        
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            # Authenticated
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+    
+        abort(401, message="Invalid credentials.")
 
 
 @blp.route("/user/<int:user_id>")
